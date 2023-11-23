aaa22ac646773073069e124cab60ccf2ac68c197
Stepan Baranov (aka Rosmir) <stepan@baranov.dev>
2023-11-23 01:11:47 +0000

    ASW CodeWhisperer has generated new code for Secret mode

--- AWS_Learning/Cloud9/hello-cloud9.js
+++ AWS_Learning/Cloud9/hello-cloud9.js
@@ -1,21 +1,46 @@
 // code example from https://docs.aws.amazon.com/cloud9/latest/user-guide/tutorial-tour-ide.html
 // run code by typing: node hello-cloud9.js
 var readline = require('readline-sync');
-var i = 10;
+var i = 0;
+// add random number
+i += Math.random() * 10;
 var input;
 
 console.log("Hello Cloud9!");
-console.log("i is " + i);
+console.log("randomized i is " + i);
 
 do {
 	    input = readline.question("Enter a number (or 'q' to quit): ");
-	    if (input === 'q') {
-		            console.log('OK, exiting.')
-		        }
-	    else{
-		            i += Number(input);
-		            console.log("i is now " + i);
-		        }
+     if (input === 'q') {
+     	console.log('OK, exiting.')
+     }
+     if (input === 's') {
+     	console.log("Pss! It's a secret mode.");
+     	// create code for ASCII face in JS
+     	console.log("  _______");
+     	console.log(" | o o o |");
+     	console.log(" |  <>  |");
+     	console.log(" |_______|");
+     	// create code to display PID
+     	console.log("PID: " + process.pid);
+     	console.log("UID: " + process.getuid());
+     	console.log("GID: " + process.getgid());
+     	console.log("cwd: " + process.cwd());
+     	console.log("version: " + process.version);
+     	console.log("platform: " + process.platform);
+     	console.log("arch: " + process.arch);
+     	console.log("uptime: " + process.uptime());
+     	console.log("memory usage: " + process.memoryUsage());
+     	console.log("execPath: " + process.execPath);
+     	console.log("execArgv: " + process.execArgv);
+     	console.log("title: " + process.title);
+     }
+     	
+     
+     else {
+     	i += Number(input);
+     	console.log("i is now " + i);
+     }
 } while (input != 'q');
 
 console.log("Goodbye!");
